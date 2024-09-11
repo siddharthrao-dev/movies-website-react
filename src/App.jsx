3636eea6a12bb464c1552639466d9c7eff8cc79f
raosiddharth2023 <146343808+raosiddharth2023@users.noreply.github.com>
2024-09-11 20:53:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Movies from './movies/Movies'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Movies />
+      </div>
+    </>
+  )
+}
+
+export default App
